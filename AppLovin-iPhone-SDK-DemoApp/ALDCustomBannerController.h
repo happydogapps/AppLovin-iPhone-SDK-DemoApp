2f494ff1549b85b4fda6bbe85ae2cfc4f61cc603
Basil Shikin <bshikin@me.com>
2012-04-30 16:48:14 -0700

    Minor project restructuring, adding custom banner example

---
+++ AppLovin-iPhone-SDK-DemoApp/ALDCustomBannerController.h
@@ -0,0 +1,13 @@
+//
+//  ALDCustomBannerController.h
+//  AppLovin-iPhone-SDK-DemoApp
+//
+//  Created by Basil on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALDCustomBannerController : UIViewController
+
+@end
