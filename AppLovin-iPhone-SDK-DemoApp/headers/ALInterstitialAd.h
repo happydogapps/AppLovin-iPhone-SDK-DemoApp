5dbea1dae0d1b2e4cc2c71c14f85b617dcf1dc53
David Anderson <leakeddave@me.com>
2013-01-23 13:44:36 -0800

    SDK 1.3.0, added initializeSDK to AppDelegate didFinishLaunching

---
+++ AppLovin-iPhone-SDK-DemoApp/headers/ALInterstitialAd.h
@@ -0,0 +1,42 @@
+//
+//  ALInterstitialAd.h
+//  sdk
+//
+//  Created by Basil on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import "ALSdk.h"
+#import "ALAdService.h"
+
+@interface ALInterstitialAd : UIView
+
+@property (strong, atomic) id<ALAdLoadDelegate> adLoadDelegate;
+@property (strong, atomic) id<ALAdDisplayDelegate> adDisplayDelegate;
+
+
+/**
+ * Show a new interstitial ad. This method would display a dialog on top of current
+ * view with an advertisement in it.
+ *
+ * @param window  A window to show the interstitial over
+ */
++(ALInterstitialAd *) showOver:(UIWindow *)window;
+
+
+/**
+ * Dismiss current dialog
+ */
+- (void)dismiss;
+
+
+/**
+ * Show current interstitial over a given window.
+ *
+ * @param window An instance of window to show the interstitial over.
+ * @param ad The ALAd that you want to render into this interstitial.
+*/
+
+-(void) showOver:(UIWindow *)window andRender: (ALAd *) ad;
+
+@end
